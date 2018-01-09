0fa065866bab46bb2449befc971c0fa19c7a6d97
Andrew Zhu <mommothazaz123@gmail.com>
2018-01-09 09:44:29 -0800

    add navx lib

--- src/frc/team2144/subsystems/Gyro.java
+++ src/frc/team2144/subsystems/Gyro.java
@@ -1,5 +1,7 @@
 package frc.team2144.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Gyro extends Subsystem {
@@ -7,6 +9,15 @@ public class Gyro extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    private AHRS navx = new AHRS(SPI.Port.kMXP);
+
+    public void resetOrientation() {
+        navx.reset();
+    }
+
+    public double getOrientation() {
+        return navx.getAngle();
+    }
 
     public void initDefaultCommand() {
         // TODO: Set the default command, if any, for a subsystem here. Example:
