9804fc2019626ec5a94288bb6c3aa225049651d2
Andrew Zhu <mommothazaz123@gmail.com>
2018-01-20 13:24:46 -0800

    add basic test

---
+++ test/frc.team2144/RobotTest.java
@@ -0,0 +1,11 @@
+package frc.team2144;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RobotTest {
+    @Test
+    public void robot1() {
+        assertTrue(true);
+    }
+}
